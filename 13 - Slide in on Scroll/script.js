79c3a4696e3f257ab9113f0f808084bd84ee8148
Teddy Aboim <teddy.aboim@gmail.com>
2024-04-28 09:34:56 +0200

    day 13 done

---
+++ 13 - Slide in on Scroll/script.js
@@ -0,0 +1,37 @@
+const images = document.querySelectorAll(".slide-in");
+
+window.addEventListener("scroll", debounce(checkSlide));
+
+function checkSlide(e) {
+  images.forEach((image) => {
+    // half-way through the image
+    const slideInAt = window.scrollY + window.innerHeight - image.height / 2;
+    // bottom of the image
+    const imageBottom = image.offsetTop + image.height;
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      image.classList.add("active");
+    } else {
+      image.classList.remove("active");
+    }
+  });
+}
+
+// This is to avoid successive call to the function at each pixel scrolled past
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
