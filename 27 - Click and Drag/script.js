301ec860173a3c74cdd25ded2d41677f13baac57
TeddyAgt <teddy.aboim@gmail.com>
2024-05-22 16:27:09 +0200

    day 27 done

---
+++ 27 - Click and Drag/script.js
@@ -0,0 +1,35 @@
+const slider = document.querySelector(".items");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+function handleMouseDown(e) {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+  slider.classList.add("active");
+}
+
+function handleMouseLeave() {
+  isDown = false;
+  slider.classList.remove("active");
+}
+
+function handleMouseUp() {
+  isDown = false;
+  slider.classList.remove("active");
+}
+
+function handleMouseMove(e) {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = x - startX;
+
+  slider.scrollLeft = scrollLeft - walk;
+}
+
+slider.addEventListener("mousedown", handleMouseDown);
+slider.addEventListener("mouseleave", handleMouseLeave);
+slider.addEventListener("mouseup", handleMouseUp);
+slider.addEventListener("mousemove", handleMouseMove);
